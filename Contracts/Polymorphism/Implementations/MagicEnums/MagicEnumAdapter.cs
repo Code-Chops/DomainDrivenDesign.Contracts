da23fd20184adfc624e699fb65b33fd1a4c14ea5
Max Bergman <maxbergman@outlook.com>
2022-07-14 11:32:20 +0200

    0.4.1: Added non-generic MagicEnum adapter.

--- Contracts/Polymorphism/Implementations/MagicEnums/MagicEnumAdapter.cs
+++ Contracts/Polymorphism/Implementations/MagicEnums/MagicEnumAdapter.cs
@@ -4,11 +4,26 @@ using CodeChops.MagicEnums;
 
 namespace CodeChops.DomainDrivenDesign.Contracts.Polymorphism.Implementations.MagicEnums;
 
-public record MagicEnumAdapter<TMagicEnum> : Adapter<MagicEnumContract>
+public record MagicEnumAdapter<TMagicEnum>() : MagicEnumAdapter(typeof(TMagicEnum)) 
 	where TMagicEnum : IMagicEnum
 {
 	protected internal override Type GetDomainObjectType() => typeof(TMagicEnum);
-	
+}
+
+public record MagicEnumAdapter : Adapter<MagicEnumContract>
+{
+	protected internal override Type GetDomainObjectType() => this.MagicEnumType;
+	private Type MagicEnumType { get; }
+	private MethodInfo GetSingleMemberMethod { get; }
+
+	public MagicEnumAdapter(Type magicEnumType)
+	{
+		this.MagicEnumType = magicEnumType;
+		this.GetSingleMemberMethod = this.MagicEnumType
+			.GetMethods(bindingAttr: BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy)
+			.Single(method => method.Name == nameof(MagicEnumDummy.GetSingleMember) && method.GetParameters().Any(parameter => parameter.ParameterType == typeof(string)));
+	}
+
 	protected internal override MagicEnumContract ConvertDomainObjectToContract(IDomainObject domainObject)
 	{
 		var contract = new MagicEnumContract((IMagicEnum)domainObject);
@@ -17,15 +32,13 @@ public record MagicEnumAdapter<TMagicEnum> : Adapter<MagicEnumContract>
 	
 	protected internal override IDomainObject ConvertContractToDomainObject(Contract contract)
 	{
-		var magicEnumContract = contract as MagicEnumContract ?? throw new ArgumentException($"Contract should be of type {nameof(MagicEnumContract)} in {nameof(MagicEnumAdapter<TMagicEnum>)}."); 
+		var magicEnumContract = contract as MagicEnumContract ?? throw new ArgumentException($"Contract should be of type {nameof(MagicEnumContract)} in {this.GetType().Name}."); 
 
-		var magicEnum = (IMagicEnum)GetSingleMemberMethod.Invoke(obj: null, parameters: new object?[] { magicEnumContract.Name })!;
+		var magicEnum = (IMagicEnum)this.GetSingleMemberMethod.Invoke(obj: null, parameters: new object?[] { magicEnumContract.Name })!;
 		return magicEnum;
 	}
 	
 	// ReSharper disable once ClassNeverInstantiated.Local
 	private record MagicEnumDummy : MagicEnum<MagicEnumDummy>;
 
-	private static MethodInfo GetSingleMemberMethod { get; } = typeof(TMagicEnum).GetMethods(bindingAttr: BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy)
-		.Single(method => method.Name == nameof(MagicEnumDummy.GetSingleMember) && method.GetParameters().Any(parameter => parameter.ParameterType == typeof(string)));
 }
