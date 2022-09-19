e847f2040fbb88bd3c86dadeb1213a20d05f5117
Max Bergman <maxbergman@outlook.com>
2022-09-19 13:19:03 +0200

    0.9.1: Re-added polymorphic point tests.

---
+++ Contracts.UnitTests/Polymorphism/PointMock.cs
@@ -0,0 +1,8 @@
+﻿using CodeChops.Geometry.Space.Points;
+
+namespace CodeChops.DomainDrivenDesign.Contracts.UnitTests.Polymorphism;
+
+public class PointMock
+{
+	public Point<double> Point { get; init; }
+}
---
+++ Contracts.UnitTests/Polymorphism/PointTests.cs
@@ -0,0 +1,36 @@
+﻿using CodeChops.DomainDrivenDesign.Contracts.Polymorphism;
+using CodeChops.GenericMath.Serialization;
+
+namespace CodeChops.DomainDrivenDesign.Contracts.UnitTests.Polymorphism;
+
+public class PointTests
+{
+	private static PointMock PointMock { get; } = new() { Point = (1D, 7D) };
+	private const string Json = "{\"Point\":{\"X\":1,\"Y\":7}}";
+	private static JsonSerializerOptions JsonSerializerOptions { get; } = new()
+	{
+		WriteIndented = false,
+		Converters =
+		{
+			new PolymorphicJsonConverter(PointContractEnum.GetValues().Select(value => value.UninitializedInstance)), new NumberJsonConverterFactory()
+		}
+	};
+
+	[Fact]
+	public void Deserialization_Point_IsCorrect()
+	{
+		var contract = JsonSerializer.Deserialize<PointMock>(Json, JsonSerializerOptions)!;
+		Assert.NotNull(contract);
+
+		Assert.Equal(typeof(PointMock), contract.GetType());
+		Assert.Equal(PointMock.Point, contract.Point);
+		Assert.Equal(PointMock.Point.X.GetType(), contract.Point.X.GetType());
+	}
+	
+	[Fact]
+	public void Serialization_Point_IsCorrect()
+	{
+		var json = JsonSerializer.Serialize(PointMock, JsonSerializerOptions);
+		Assert.Equal(Json, json);
+	}
+}
