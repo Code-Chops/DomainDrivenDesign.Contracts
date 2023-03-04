0158f9b99928d725f6be8c5a2fb54c2e0b687b9d
Max Bergman <maxbergman@outlook.com>
2023-03-04 17:43:33 +0100

    0.16.0: Added collection contract and paging.

---
+++ Contracts/CollectionContract.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+
+namespace CodeChops.Contracts;
+
+public record CollectionContract<TElementContract> : Contract, IReadOnlyList<TElementContract>
+	where TElementContract : Contract
+{
+	public IReadOnlyList<TElementContract> Values { get; }
+
+	public PagingContract Paging { get; }
+	
+	public int Count => this.Values.Count;
+
+	public CollectionContract(IReadOnlyList<TElementContract> values, PagingContract paging)
+	{
+		this.Values = values;
+		this.Paging = paging;
+	}
+
+	public virtual TElementContract this[int index] 
+		=> Validator.Get<TElementContract>.Default.GuardIndexInRange(this.Values, index, errorCode: null)!;
+	
+	IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+	public IEnumerator<TElementContract> GetEnumerator() => this.Values.GetEnumerator();
+}
---
+++ Contracts/PagingContract.cs
@@ -0,0 +1,32 @@
+﻿namespace CodeChops.Contracts;
+
+/// <summary>
+/// A paging contract with a default (maximum page) size that can be overriden.
+/// </summary>
+// ReSharper disable once ClassWithVirtualMembersNeverInherited.Global
+public record PagingContract : Contract
+{
+	[JsonIgnore] public virtual uint MaximumSize => 1000;
+	[JsonIgnore] public virtual uint DefaultSize => 20;
+
+	/// <summary>
+	/// The page number (starting with page 0).
+	/// </summary>
+	public uint Page { get; } 
+	
+	/// <summary>
+	/// The page size.
+	/// </summary>
+	public uint Size { get; }
+
+	[JsonConstructor]
+	// ReSharper disable VirtualMemberCallInConstructor
+	public PagingContract(uint page, uint? size)
+	{
+		this.Page = page;
+		this.Size = size ?? this.DefaultSize;
+
+		if (this.Size == 0) this.Size = 1;
+		if (this.Size > this.MaximumSize) this.Size = this.MaximumSize;
+	}
+}
